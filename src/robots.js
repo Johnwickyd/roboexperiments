9c41800ac4d1a5cc918c6548724efac9840e587b
Johnwickyd <109072120+Johnwickyd@users.noreply.github.com>
2022-08-27 20:06:40 +0300

    hi

---
+++ src/robots.js
@@ -0,0 +1,62 @@
+export const robots = [
+    {
+        id: 1,
+        name: 'Nathnael Teshome',
+        username: 'nate',
+        email: 'nathnael.teshome@gmail.com'
+    },
+    {
+        id: 2,
+        name: 'Feven Teshome',
+        username: 'feve',
+        email: 'feven.teshome@yahoo.com'
+    },
+    {
+        id: 3,
+        name: 'Naruto Uzumaki',
+        username: 'naru',
+        email: 'naruto.uzumaki@gtx.io'
+    },
+    {
+        id: 4,
+        name: 'Kevin Debruyne',
+        username: 'kevi',
+        email: 'kevin.Debruyne@lex.org'
+    },
+    {
+        id: 5,
+        name: 'Abdurazak Feki',
+        username: 'feki',
+        email: 'abdurezak.feki@lex.org'
+    },
+    {
+        id: 6,
+        name: 'Tony Stark',
+        username: 'Tony',
+        email: 'Tony.Stark@yahoo.io'
+    },
+    {
+        id: 7,
+        name: 'Brook Worku',
+        username: 'Broo',
+        email: 'Brook.Worku@yahoo.com'
+    },
+    {
+        id: 8,
+        name: 'Jonny English',
+        username: 'Jonn',
+        email: 'Jonny.English@lex.org'
+    },
+    {
+        id: 9,
+        name: 'Tom Cruise',
+        username: 'Tom',
+        email: 'Tom.Cruise@gtx.io'
+    },
+    {
+        id: 10,
+        name: 'Aizen Sosuke',
+        username: 'aize',
+        email: 'aizen.sosuke@gmail.com'
+    }
+]
